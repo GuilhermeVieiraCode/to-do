9b981875142adaddd7b5f61e8adb93f32cd46a33
Guilherme <alves.guilherme1357@gmail.com>
2021-04-11 15:43:21 -0300

    card de criaçao de tarefa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CriarTarefaComponent } from './tarefa/criar-tarefa/criar-tarefa.component';
+import { TarefaModule } from './tarefa/tarefa.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TarefaModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tarefa/criar-tarefa/criar-tarefa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-tarefa',
+  templateUrl: './criar-tarefa.component.html',
+  styleUrls: ['./criar-tarefa.component.scss']
+})
+export class CriarTarefaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
